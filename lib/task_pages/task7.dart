000641a7d085bd1b81f68bf5637aed95453d90fc
Gautham Krishnan <gauthamkrishnanpriya@gmail.com>
2022-03-20 12:34:24 +0530

    ex api working

--- lib/task_pages/task7.dart
+++ lib/task_pages/task7.dart
@@ -1,4 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/3'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
 
 class TaskSeven extends StatefulWidget {
   const TaskSeven({Key? key}) : super(key: key);
@@ -7,28 +45,77 @@ class TaskSeven extends StatefulWidget {
   _TaskDashboardState createState() => _TaskDashboardState();
 }
 
-
 class _TaskDashboardState extends State<TaskSeven> {
+  final _playlistTextController = TextEditingController();
+  var _text = TimeOfDay.now().toString();
+  late Future<Album> futureAlbum;
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  void updateTOD(String txt) {
+    _text = TimeOfDay.now().toString();
+    //_text = http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')).toString();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Task-7',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.yellow.shade800,
-        elevation: 1,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.white,
+        appBar: AppBar(
+          title: const Text(
+            'Task-7',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.yellow.shade800,
+          elevation: 1,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
           ),
         ),
-      ),
-    );
+        body: Container(
+          child: Column(children: <Widget>[
+            Flexible(
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _playlistTextController,
+                  onSubmitted: updateTOD,
+                  decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 77, 201, 81),
+                            width: 2.0),
+                      ),
+                      hintText: 'Enter Plalist Name'),
+                  //onSubmitted: (playlistTextController.text) {},
+                ),
+              ),
+            ),
+            Center(
+              child: FutureBuilder<Album>(
+                future: futureAlbum,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data!.title);
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  }
+
+                  // By default, show a loading spinner.
+                  return const CircularProgressIndicator();
+                },
+              ),
+            ),
+          ]),
+        ));
   }
 }
